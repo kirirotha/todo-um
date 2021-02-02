f4674886afb78e36341988f05becdfeae62ec76c
kirirotha <kirirotha@gmail.com>
2021-02-01 20:45:58 -0600

    added delete to frontend and backend

--- backend/restful-web-services/src/main/java/com/todoum/rest/webservices/restfulwebservices/todo/ToDoResource.java
+++ backend/restful-web-services/src/main/java/com/todoum/rest/webservices/restfulwebservices/todo/ToDoResource.java
@@ -3,12 +3,16 @@ package com.todoum.rest.webservices.restfulwebservices.todo;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.todoum.rest.webservices.restfulwebservices.todo.Todo;
 
+@CrossOrigin(origins="http://localhost:4200")
 @RestController
 public class ToDoResource {
 
@@ -19,6 +23,17 @@ public class ToDoResource {
 	@GetMapping("/users/{username}/todos")
 	public List<Todo> getAllTodos(@PathVariable String username){
 		return todoService.findAll();
+	}
+	
+	@DeleteMapping("/users/{username}/todos/{id}")
+	public ResponseEntity<Void> deleteTodo(
+			@PathVariable String username, @PathVariable long id){
+		Todo todo = todoService.deleteById(id);
+		if(todo != null) {
+			return ResponseEntity.noContent().build();
+		}
+		
+		return ResponseEntity.notFound().build();
 	}
 	
 }
--- frontend/src/app/list-todos/list-todos.component.ts
+++ frontend/src/app/list-todos/list-todos.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { TodoDataService } from '../service/data/todo-data.service';
 
 export class Todo {
   constructor(
@@ -19,17 +20,43 @@ export class ListTodosComponent implements OnInit {
   //   id:1,
   //   description:'lalalala'
   // }
-  todos =[
-    new Todo(1, 'Mow the Lawn', false, new Date()),
-    new Todo(2, 'Walk the Dog', false, new Date()),
-    new Todo(3, 'Cook Dinner', false, new Date()),
-    new Todo(4, 'Wash the Car', false, new Date()),
+  // todos =[
+  //   new Todo(1, 'Mow the Lawn', false, new Date()),
+  //   new Todo(2, 'Walk the Dog', false, new Date()),
+  //   new Todo(3, 'Cook Dinner', false, new Date()),
+  //   new Todo(4, 'Wash the Car', false, new Date()),
     
-  ];
+  // ];
 
-  constructor() { }
+  todos: Todo[];
+
+  message: string;
+
+  constructor(
+    private todoService: TodoDataService
+  ) { }
 
   ngOnInit(): void {
+    this.refreshTodos();
   }
 
+  deleteTodo(id){
+    console.log(`delete id ${id}`);
+    this.todoService.deleteTodo('Levi', id).subscribe(
+      response =>{
+        console.log(response);
+        this.message = `Delete of Todo id:${id} successful!`
+        this.refreshTodos();
+      }
+    )
+  }
+
+  refreshTodos(){
+    this.todoService.retrieveAllTodos('Levi').subscribe(
+      response =>{
+        this.todos = response;
+        console.log(response);
+      }
+    )
+  }
 }
